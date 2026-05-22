64b6561bbf156d3d99828e5b61774eb4c624da0c
darren277 <darren277@yahoo.com>
2026-05-22 07:03:32 -0700

    More assorted benchmark stuff WIP.

--- bench/settings.py
+++ bench/settings.py
@@ -7,6 +7,10 @@ dotenv.load_dotenv()
 import os
 
 
+
+MIMIC_CSV_PATH = os.getenv("MIMIC_CSV_PATH", "data/mimic_data.csv")
+
+
 API_URL = "http://localhost:3123"
 
 
@@ -16,3 +20,9 @@ AMT_API_KEY = os.getenv("AMT_API_KEY")
 
 # http://localhost:3123/api/debug/session_v2
 SESSION_TOKEN = os.getenv("SESSION_TOKEN")
+
+
+
+p = r'D:\testing\work\dm\arsmedicatech-evals\MedAgentBench\data\medagentbench\test_data_v2.json'
+p = r"C:\Users\Darren\Desktop\MAIN\currentz\amt\evals\train.jsonl"
+
